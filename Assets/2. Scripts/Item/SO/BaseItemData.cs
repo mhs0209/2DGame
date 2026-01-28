7999a74a5b12dae4c24bc9610fb922244b0141b5
DESKTOP-ICE249B\mhs99 <https://github.com/mhs0209>
2026-01-28 23:35:34 +0900

    [테이블 작업 진행 전 불필요한 정보 삭제]
    테이블에 프리팹 및 SO의 모든 정보를 담는 것이 아닌 분류에 필요한 정보만 담고 이를 통해 프리팹을 불러오는 등의 작업을 진행할 예정.

--- Assets/2. Scripts/Item/SO/BaseItemData.cs
+++ Assets/2. Scripts/Item/SO/BaseItemData.cs
@@ -7,9 +7,9 @@ public abstract class ItemData : ScriptableObject
     [Header("Base Info")]
     public int itemID;
     public string itemName;
-    [TextArea] public string description;
+    //[TextArea] public string description;
     //public RoomType category;
-    public Sprite itemIcon;
+    //public Sprite itemIcon;
     public ItemType type;
     public GameObject itemPrefab; // 필드에 드랍될 때의 외형
 }
