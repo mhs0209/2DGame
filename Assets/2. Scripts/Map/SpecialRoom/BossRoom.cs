94acdd489d25e9fa5195ab1b836d7b60b77b0dda
DESKTOP-ICE249B\mhs99 <https://github.com/mhs0209>
2026-02-09 20:05:17 +0900

    [리롤 액티브 버그 수정]
    보스방 아이템 안 돌아가는 문제 수정(원인: 방에 자식으로 존재하는 아이템을 돌리는 로직이었으나 보스방 아이템은 방에 자식으로 존재하지 않았음)

--- Assets/2. Scripts/Map/SpecialRoom/BossRoom.cs
+++ Assets/2. Scripts/Map/SpecialRoom/BossRoom.cs
@@ -10,7 +10,7 @@ public class BossRoom : ItemRoom
         if (controller.roomData is BossMap data) {
             // 보상 아이템 생성
             if (data.itemDropPool.Count > 0)
-                Instantiate(data.itemDropPool[Random.Range(0, data.itemDropPool.Count)].itemPrefab, transform.position + Vector3.up, Quaternion.identity);
+                Instantiate(data.itemDropPool[Random.Range(0, data.itemDropPool.Count)].itemPrefab, transform.position + Vector3.up, Quaternion.identity, transform);
             
             // 포탈 생성
             if (stagePortalPrefab != null)
